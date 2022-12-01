2d751499552bc2750fac2259ef4f0d97766fa2fa
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2022-12-01 10:31:24 -0300

    Layout Parcial com styled-components

---
+++ src/components/FlashMain.js
@@ -0,0 +1,88 @@
+import { useState } from "react"
+import styled from 'styled-components'
+import logo from "../assets/img/logo.png"
+
+const cards = [
+	{ question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
+	{ question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+	{ question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
+	{ question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+	{ question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
+	{ question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
+	{ question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
+	{ question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+]
+export default function FlashMain(){
+    return (
+        <ScreenContainer>
+            <LogoContainer>
+                <img src={logo} alt="zap logo" />
+                <h1>
+                    Zaprecall
+                </h1>
+            </LogoContainer>
+
+            <Footer>
+                X/Y Concluídos
+                <FooterBotoes>
+
+                </FooterBotoes>
+            </Footer>
+        </ScreenContainer>
+    )
+}
+
+const ScreenContainer = styled.div`
+    background-color: #FB6B6B;
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0px;
+    padding: 0px;
+    padding-bottom: 200px;
+`
+
+const LogoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 40px 0 20px 0;
+    
+    img {
+        width: 52px;
+    }
+    h1 {
+        font-family: 'Righteous';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 36px;
+        line-height: 45px;
+        color: #FFFFFF;
+        margin-left: 20px;
+    }
+`
+
+const Footer = styled.div`
+    width: 100%;
+    min-height: 50px;
+    background-color: #FFFFFF;
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Recursive';
+    font-weight: 400;
+    font-size: 18px;
+    color: #333333;
+    padding: 10px;
+`
+
+const FooterBotoes = styled.div`
+    display: flex;
+    width: 80%;
+    justify-content: space-between;
+    margin: 20px;
+`
